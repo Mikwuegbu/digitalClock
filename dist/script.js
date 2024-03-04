237eb9d3f01e8200ef089e1111c38ca8fcb7b77a
Mikwuegbu <mstephen130@yahoo.com>
2024-03-04 08:34:45 +0100

    completed

--- dist/script.js
+++ dist/script.js
@@ -1,22 +1,42 @@
+let paused = false;
+let interVal;
+
 //updating the time
 
 const timeUpdate = () => {
-	const now = new Date();
-	let hours = now.getHours();
-	let minutes = now.getMinutes();
-	let seconds = now.getSeconds();
+	if (!paused) {
+		const now = new Date();
+		let hours = now.getHours();
+		let minutes = now.getMinutes();
+		let seconds = now.getSeconds();
 
-	// Ensure hours, minutes, and seconds are displayed in two digits
-	hours = hours < 10 ? '0' + hours : hours;
-	minutes = minutes < 10 ? '0' + minutes : minutes;
-	seconds = seconds < 10 ? '0' + seconds : seconds;
+		// Ensure hours, minutes, and seconds are displayed in two digits
+		hours = hours < 10 ? '0' + hours : hours;
+		minutes = minutes < 10 ? '0' + minutes : minutes;
+		seconds = seconds < 10 ? '0' + seconds : seconds;
 
-	const timeString = hours + ':' + minutes + ':' + seconds;
-	document.getElementById('timer').innerText = timeString;
+		const timeString = hours + ':' + minutes + ':' + seconds;
+		document.getElementById('timer').innerText = timeString;
+
+		if (hours >= 17 && hours <= 20) {
+			document.body.style.backgroundColor = '#808080';
+		} else {
+			document.body.style.backgroundColor = '#fff';
+		}
+	}
 };
 
-// Update the clock every second
-setInterval(timeUpdate, 1000);
+const stopBtnFn = () => {
+	paused = true;
+	clearInterval();
+};
 
-// Initial call to display the clock immediately
+const startBtnFn = () => {
+	paused = false;
+	updateClock();
+};
+
+setInterval(timeUpdate, 1000);
+document.getElementById('stopBtn').addEventListener('click', stopBtnFn);
+document.getElementById('startBtn').addEventListener('click', startBtnFn);
 updateClock();
